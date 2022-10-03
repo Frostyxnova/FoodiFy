37ac28686ccfb79b97b9e755f404c8f033b35cd8
Farhan Khan <90808223+Frostyxnova@users.noreply.github.com>
2022-10-03 11:01:08 +0530

    Initial commit

---
+++ app/src/main/java/com/farhan/foodify/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.farhan.foodify.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.farhan.foodify.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
